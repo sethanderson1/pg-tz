d6328b31aa6ed720fb7365eb393367b61f4999b6
Seth Anderson <sdande02@gmail.com>
2020-07-21 00:59:44 -0700

    t

--- src/app.js
+++ src/app.js
@@ -23,74 +23,75 @@ app.use(helmet());
 // );
 app.use(cors())
 
-app.get('/', async (req, res) => {
+// app.get('/', async (req, res) => {
+
+//     const knex = req.app.get('db')
+
+//     // const dateNow = dayjs()
+//     const dateNow = dayjs().format() 
+//     // const dateNow = dayjs().utc().format()
+//     // const dateNow = new Date()
+//     console.log('dateNow', dateNow)
+
+//     const newDate = {
+//         timestamp: dateNow,
+//         timestamptz: dateNow
+//     }
+//     // await knex.schema.raw('SET timezone="UTC";')
+//     datesReturnedFromInsert = await knex
+//         .insert(newDate)
+//         .into('dates')
+//         .returning('*')
+//         .then(rows => {
+//             return rows[0]
+//         });
+
+//     console.log('datesReturnedFromInsert', datesReturnedFromInsert)
+//     // const localDate = dayjs(datesReturnedFromInsert.timestamptz).format()
+//     // const localDate = dayjs(datesReturnedFromInsert.timestamptz).format()
+//     // datesReturnedFromInsert.localDate = localDate
+//     // console.log('localDate', localDate)
+
+
+
+//     res.send(datesReturnedFromInsert);
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.post('/', jsonParser, async (req, res) => {
 
     const knex = req.app.get('db')
 
     // const dateNow = dayjs()
-    const dateNow = dayjs().format() 
+    // const dateNow = dayjs().format()
     // const dateNow = dayjs().utc().format()
     // const dateNow = new Date()
-    console.log('dateNow', dateNow)
+    // console.log('dateNow', dateNow)
 
-    const newDate = {
-        timestamp: dateNow,
-        timestamptz: dateNow
-    }
-    // await knex.schema.raw('SET timezone="UTC";')
-    datesReturnedFromInsert = await knex
-        .insert(newDate)
-        .into('dates')
-        .returning('*')
-        .then(rows => {
-            return rows[0]
-        });
-
-    console.log('datesReturnedFromInsert', datesReturnedFromInsert)
-    // const localDate = dayjs(datesReturnedFromInsert.timestamptz).format()
-    // const localDate = dayjs(datesReturnedFromInsert.timestamptz).format()
-    // datesReturnedFromInsert.localDate = localDate
-    // console.log('localDate', localDate)
-
-
-
-    res.send(datesReturnedFromInsert);
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.post('/', jsonParser,async (req, res) => {
-
-    const knex = req.app.get('db')
-
-    // const dateNow = dayjs()
-    const dateNow = dayjs().format() 
-    // const dateNow = dayjs().utc().format()
-    // const dateNow = new Date()
-    console.log('dateNow', dateNow)
-
+    console.log('req.body.timestamp', req.body.timestamp)
     const newDate = {
         timestamp: req.body.timestamp,
         timestamptz: req.body.timestamp
